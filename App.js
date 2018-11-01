07fdecaed4160e225e896b1ec889b5ed44ca8f0d
Neil Vinod <neilvinod14@gmail.com>
2018-11-01 15:22:58 -0700

    random app.js

--- App.js
+++ App.js
@@ -7,7 +7,6 @@ import RegisterScreen from './components/Register'
 import AuthHomeScreen from './components/AuthHome'
 import AuthApprovalScreen from './components/AuthApproval'
 
-//stack navigator (list of all screens) to facilitate transitions
 
 
 export default createStackNavigator({
