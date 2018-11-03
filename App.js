9f78dca09cf6ee0c9c02b85ca4b55164f3dcdbf3
Neil Vinod <neilvinod14@gmail.com>
2018-11-03 14:19:29 -0700

    unnecessary comments removed

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
-import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+import { createStackNavigator } from 'react-navigation'; 
 import SplashScreen from './components/Splash'
 import LoginScreen from './components/Login'
 import RegisterScreen from './components/Register'
